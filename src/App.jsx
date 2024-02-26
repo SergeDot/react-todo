a1fb8b20bc81131309fcd7cbb22d8b9e7ad822a7
SergeDot <srg@dr.com>
2024-02-26 16:55:29 -0500

    broke down the components AddTodoForm and TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,20 @@
 import './App.css'
-
-const todoList = [{
-  id:1,
-  title:"451 Fahrenheit",
-  author:"Ray Bradbury",
-  state: "California",
-  country:"USA"
-  }, {
-  id:2,
-  title:"The Catcher in the Rye",
-  author:"Jerome David Sallinger",
-  state:"New Hampshire",
-  country:"USA"
-  }, {
-  id:3,
-  title:"The Lord of Flies",
-  author:"William Golding",
-  state:"Cornwall",
-  country:"UK"
-}];
+import TodoList from './TodoList'
+import AddTodoForm from './AddTodoForm'
 
 function App() {
-  
+
   return (
     <>
       <h1>
         Todo List
       </h1>
-      <ul>
-        {todoList.map(item=>{
-          return <li key={item.id}>{item.title}</li>
-        })}
-      </ul>
+      <AddTodoForm />
+      <TodoList />
     </>
   )
 }
+
+
 
 export default App
