8974d72a2134555cca773f85dbb165495c486c7e
SergeDot <srg@dr.com>
2024-02-27 21:16:46 -0500

    added addTodoItem functionality with state hooks

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,16 @@
-const AddTodoForm = () => {
+const AddTodoForm = (props) => {
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.title.value;
+    console.log(todoTitle);
+    event.target.reset();
+    props.onAddTodo(todoTitle)
+  }
 
   return (
-    <form action="">
+    <form action="submit" onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle" />
+      <input type="text" id="todoTitle" name="title" />
       <button>Add</button>
     </form>
   )
---
+++ src/TodoListItem.jsx
@@ -0,0 +1,8 @@
+const TodoListItem = (props) => {
+
+  return (
+    <li>{props.todo.title}</li>
+  )
+}
+
+export default TodoListItem
