278804b2fbc74b71ac7c0617b2889e55fab37831
SergeDot <srg@dr.com>
2024-04-09 12:55:59 -0400

    Lesson 1.8, week 10. Added GET and POST, pessimistic rendering on adding, check for valid entries

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -2,25 +2,33 @@ import { useState } from 'react';
 import InputWithLabel from './InputWithLabel';
 
 const AddTodoForm = ({ onAddTodo }) => {
+  const [todoTitle, setTodoTitle] = useState([]);
+  const [isInvalid, setIsInvalid] = useState(true);
+
   const handleTitleChange = (event) => {
     const newTodoTitle = event.target.value;
     setTodoTitle(newTodoTitle);
+    
+    // check for empty string
+    if (newTodoTitle && newTodoTitle.trim()) {
+      setIsInvalid(false);
+    } else {
+      setIsInvalid(true);
+    }
   };
 
   const handleAddTodo = (event) => {
     event.preventDefault();
-    onAddTodo({ title: todoTitle, id: Date.now() });
+    onAddTodo({ title: todoTitle });
     setTodoTitle('');
   };
-
-  const [todoTitle, setTodoTitle] = useState([]);
 
   return (
     <form action="submit" onSubmit={handleAddTodo}>
       <InputWithLabel todoTitle={todoTitle} handleTitleChange={handleTitleChange} isFocused >
         Title
       </InputWithLabel>
-      <button>Add</button>
+      <button disabled={isInvalid}>Add</button>
     </form>
   );
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,81 @@
 import './App.css';
 import TodoList from './TodoList';
 import AddTodoForm from './AddTodoForm';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+
+const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
 
 const App = () => {
 
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isAdding, setIsAdding] = useState(false);
+
+  // useCallBack for pessimistic rendering
+  const callbackFetchData = useCallback(async () => {
+    console.log(`16`);
+    const options = {
+      method: "GET",
+      headers: { Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}` },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error(`${response.status}`);
+      const { records: data } = await response.json();
+      const todos = data.map(todo => todo = { title: todo.fields.title, id: todo.id });
+      return todos;
+    } catch (error) {
+      console.log(error);
+    }
+  }, [isLoading, isAdding]);
 
   useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [] } }), 2000)
-    }).then(result => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    })
-  }, [])
+    if (isLoading) {
+      new Promise((resolve, reject) => { setTimeout(() => resolve(callbackFetchData()), 2000) })
+        .then(result => {
+          setTodoList(result || []);
+          setIsLoading(false);
+        });
+    }
+    
+    if (isAdding) {
+      new Promise((resolve, reject) => resolve(callbackFetchData()))
+        .then(result => {
+          setTodoList(result || []);
+          setIsAdding(false);
+        })
+    }
+  }, [callbackFetchData]);
 
   useEffect(() => {
     if (!isLoading) {
       localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-    };
+    }
   }, [todoList]);
 
-  const addTodo = (newTodo) => {
-    setTodoList([newTodo, ...todoList])
+  const addTodo = async (newTodo) => {
+    let todo = JSON.stringify({ "fields": newTodo });
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+      },
+      body: todo
+    };
+    const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) throw new Error(`${response.status}`);
+      const { id: todoId, fields: { title: todoTitle } } = await response.json();
+      todo = { id: todoId, title: todoTitle }
+      setIsAdding(true)
+      setTodoList([todo, ...todoList]);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const removeTodo = (id) => {
@@ -34,7 +85,6 @@ const App = () => {
 
   return (
     <>
-
       <h1>
         Todo List
       </h1>
