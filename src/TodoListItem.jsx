c817549c312508faab3fa46b9c0cec4b701834bd
SergeDot <srg@dr.com>
2024-05-12 00:09:36 -0400

    Week 13. Lesson 3.1 CSS Styled Components SVG, added CSS using modules, added SVG

--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,9 +1,14 @@
+import styles from './TodoListItem.module.css';
+import Task from './task.svg?react'
+
 const TodoListItem = ({ id, title, onRemoveTodo }) => {
 
   return (
-    <li>
-      {title}
-      &nbsp;
+    <li className={styles.ListItem}>
+      <span>
+        <Task height="30px" width="30px" />
+      </span>
+      <span className={styles.itemTitle}>{title}</span>
       <button onClick={() => onRemoveTodo(id)}>Remove</button>
     </li>
   );
